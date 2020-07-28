442608ce82ac988a741ad0cf1ad9659cae0b3e04
JoaoVitorPereiraUFU <joaovitorpessoal2@gmail.com>
2020-07-28 15:36:48 -0300

    new: adicao de routes, adicao de pages

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/logo.png';
 
 import Button from '../Button';
@@ -7,11 +8,11 @@ import './menu.css';
 function Menu() {
   return (
     <nav className="Menu">
-      <a href="/">
+      <Link to="/">
         <img src={Logo} alt="DevFlix" className='Logo'/>
-      </a>
+      </Link>
 
-      <Button as="a" href="/" className="ButtonLink">
+      <Button as={Link} to="/cadastro/video" className="ButtonLink">
         Novo vídeo
       </Button>
     </nav>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/Video'
+import CadastroCategoria from './pages/cadastro/Categoria'
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/cadastro/video" exact component={CadastroVideo} />
+      <Route path="/cadastro/categoria" exact component={CadastroCategoria} />
+      <Route component={() => (<div>Página 404</div>)} /> {/* Para casos de erro 404  */}
+
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
