e8861713329c64b8b6e505158b92bc7635f14501
JoaoVitorPereiraUFU <joaovitorpessoal2@gmail.com>
2020-07-30 20:56:26 -0300

    new: adicao de loading, useEffect, json server

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-import Home from './pages/Home';
-import CadastroVideo from './pages/cadastro/Video'
-import CadastroCategoria from './pages/cadastro/Categoria'
-
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
 
 ReactDOM.render(
   <BrowserRouter>
@@ -14,10 +13,11 @@ ReactDOM.render(
       <Route path="/" exact component={Home} />
       <Route path="/cadastro/video" exact component={CadastroVideo} />
       <Route path="/cadastro/categoria" exact component={CadastroCategoria} />
-      <Route component={() => (<div>Página 404</div>)} /> {/* Para casos de erro 404  */}
+      <Route component={() => (<div>Página 404</div>)} />
+      {' '}
+      {/* Para casos de erro 404  */}
 
     </Switch>
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
-
---
+++ src/pages/cadastro/Categoria/index.jsx
@@ -0,0 +1,105 @@
+import React, { useState, useEffect } from 'react';
+
+import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
+import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
+
+function CadastroCategoria() {
+  const valoresIniciais = {
+    nome: '',
+    descricao: '',
+    cor: '',
+  };
+  const [values, setValues] = useState(valoresIniciais);
+  const [categorias, setCategorias] = useState([]);
+
+  function setValue(chave, valor) {
+    setValues({
+      ...values,
+      [chave]: valor,
+    });
+  }
+
+  const handleChange = (e) => setValue(e.target.getAttribute('name'), e.target.value);
+
+  useEffect(() => {
+    const URL = 'http://localhost:3333/categorias';
+    fetch(URL).then(async (respostaDoServidor) => {
+      const resposta = await respostaDoServidor.json();
+      setCategorias([
+        ...resposta,
+      ]);
+    });
+  }, []);
+
+  return (
+
+    <PageDefault>
+      <h1>
+        Cadastro de Categoria :
+        {values.nome}
+        {' '}
+      </h1>
+
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          setCategorias([
+            ...categorias,
+            values,
+          ]);
+          setValues(valoresIniciais);
+        }}
+      >
+        <FormField
+          label="Nome da Categoria"
+          type="text"
+          name="nome"
+          value={values.nome}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="Descrição"
+          type="????"
+          name="descricao"
+          value={values.descricao}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="Cor"
+          type="color"
+          name="cor"
+          value={values.cor}
+          onChange={handleChange}
+        />
+
+        <Button>
+          Cadastrar
+        </Button>
+      </form>
+
+      {categorias.length === 0 && (
+        <div>
+          Loading....
+        </div>
+      )}
+
+      <ul>
+        {categorias.map((categoria) => (
+          <li key={`${categoria.nome}`}>
+            {categoria.nome}
+          </li>
+        ))}
+      </ul>
+
+      <Link to="/">
+        Ir para home
+      </Link>
+    </PageDefault>
+  );
+}
+
+export default CadastroCategoria;
